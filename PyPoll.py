0cbc28d241842bc529db9d64b35c51e4613c6031
unknown <ftjyangf@gmail.com>
2020-08-01 21:14:59 -0700

    adding Pypoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+import csv
+import os 
+os.chdir("C:\\Users\\Owner\\Desktop")
+os.getcwd()
+filename = "C:\\Users\\Owner\\Desktop\\UCD\\homeWork\\Election_Analysis\\resouce\\election_results.csv"
+data1 = open(filename, "r")
